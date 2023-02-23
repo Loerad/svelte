f9479f460b038aa46d259f9454fb09dbba6619ad
Rohan Anakin (1000114001) <ANAKRV1@student.op.ac.nz>
2023-02-24 11:56:10 +1300

    feat : Added JS code for button move

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to the cheese</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the cheese or perish</p>
+<!-- CSS - styling -->
+<style>
+    h1,p,button{
+
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+    header{
+        margin-left:50%; 
+    }
+    .flex-container
+    {
+        display: flex;
+        flex-wrap: wrap;
+        max-width: 100vw;
+        max-height: 100vh;
+    }
+</style>
+<!-- JS functions and stuff-->
+<script>
+    let count = 0;
+    let randoleft, randotop, randobottom, randoright;
+    function incrementCount()
+    {
+        randoleft = Math.floor(Math.random() * window.visualViewport.width);
+        randotop = Math.floor(Math.random() * window.visualViewport.height);
+        randobottom = Math.floor(Math.random() * window.visualViewport.height);
+        randoright = Math.floor(Math.random() * window.visualViewport.width);
+        count++;
+    }
+</script>
+<!-- HTML main stuff-->
+<div class= "flex-container">
+<button id="but"on:click={incrementCount}  style="margin:{randotop}px {randoright}px {randobottom}px {randoleft}px;">
+    <!-- If count === 1 output = time, else times-->
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+</div>
+
+
